720519b128ec623236a00e207720d9ae99e9f070
hyeseong <hyeseong7848@gmail.com>
2023-11-16 21:48:50 +0900

    Initial Commit

---
+++ MailplugAssignment/Scene/HomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomeVC.swift
+//  MailplugAssignment
+//
+//  Created by 장혜성 on 2023/11/16.
+//
+
+import UIKit
+
+final class HomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
